1f19595fb884d1628a0c08716500093188b6190c
Jialu Gu <Jialu_Gu@epam.com>
2020-03-26 17:36:19 +0800

    adsf

--- routers/home.js
+++ routers/home.js
@@ -3,6 +3,11 @@ const crypto = require('crypto');
 const router = new Router({prefix: '/home'});
 
 router.get('/', async (ctx, next) => {
+    const {code, state} = ctx.query;
+    console.log('----------------');
+    console.log(code);
+    console.log('----------------');
+    console.log(state);
     ctx.body = 'welcome';
 })
 
