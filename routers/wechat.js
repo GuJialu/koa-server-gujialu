29c51f9aec5737ff6114a82c0355e4fee7dc29d3
Jialu Gu <Jialu_Gu@epam.com>
2020-03-26 11:03:50 +0800

    confirm wechat developer

--- routers/wechat.js
+++ routers/wechat.js
@@ -1,8 +1,21 @@
 const Router = require('@koa/router');
+const crypto = require('crypto');
 const router = new Router()
 
+const token = 'gujialu';
+
 router.get('/', async (ctx, next) => {
-    console.log(ctx.request.body);
+    console.log(ctx.query);
+    const {signature, echostr, timestamp, nonce}  = ctx.query;
+    const str = [token, timestamp, nonce].sort((a, b)=>a>b).join('');
+
+    const sha1 = crypto.createHash('sha1');
+    sha1.update(str);
+    const hashedStr = sha1.digest('hex');
+
+    if(signature === hashedStr){
+        ctx.body = echostr;
+    }
 })
 
 module.exports = router;
